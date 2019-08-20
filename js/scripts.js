d72186a6b8edc3b74c801eee99fc8af0fec944f6
Cynthia Ponce and Victor Guttmann <cynthia@>
2019-08-20 14:31:13 -0700

    simplified js with .toLowerCase()

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
   $("form#form").submit(function(event) {
     var answer1 = $("input#question1").val();
-    var answer1final=answer1.toLowerCase();
     var answer2 = $("input#question2").val();
-    var answer2final=answer2.toLowerCase();
     var answer3 = $("input#question3").val();
-    var answer3final=answer3.toLowerCase();
 
-    if (answer1final === "yes") {
+    if (answer1.toLowerCase() === "yes") {
       alert("You are a conservative");
-    } else if (answer2final === "yes") {
+    } else if (answer2.toLowerCase() === "yes") {
       alert("You are a moderate");
-    } else if (answer3final === "yes"){
+    } else if (answer3.toLowerCase() === "yes"){
       alert("You are a liberal");
     } else {
       alert("Congratulations on nothing.");
